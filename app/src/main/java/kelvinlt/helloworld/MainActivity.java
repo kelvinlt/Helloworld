6a6cfa7fbac612639cd7a23fe919d35ef4ef7124
kelvinlt <kelvinleethen@gmail.com>
2017-09-18 18:41:13 +0200

    Creacion de 1er prueba para android

--- app/src/main/java/kelvinlt/helloworld/MainActivity.java
+++ app/src/main/java/kelvinlt/helloworld/MainActivity.java
@@ -2,12 +2,56 @@ package kelvinlt.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+
+    boolean opcion=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.et1);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hola "+ editText.getText());
+            }
+        });
+//        if(opcion=true){
+//            btn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    textView.setText("Hola");
+//                    opcion=false;
+//                }
+//            });
+//        }
+//        if(opcion=false){
+//            btn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    textView.setText("Adios");
+//                    opcion=true;
+//                }
+//            });
+//        }
+
+
+
+
+
     }
 }
