877575e23539900a6da6aab329ad1a025c90f529
kelvinlt <kelvinleethen@gmail.com>
2017-10-05 18:38:46 +0200

    Update de cosas hechas 05/10/2017

--- app/src/main/java/kelvinlt/helloworld/MainActivity.java
+++ app/src/main/java/kelvinlt/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package kelvinlt.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,12 +25,20 @@ public class MainActivity extends AppCompatActivity {
         Button btn1 = (Button) findViewById(R.id.btn1);
         Button btn2 = (Button) findViewById(R.id.btn2);
 
-        btn2.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                textView.setText("Hello World!");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                Log.d("flx","S'ha fet un clic");
                 textView.setText("Hola "+ editText.getText());
             }
         });
+
 //        if(opcion=true){
 //            btn.setOnClickListener(new View.OnClickListener() {
 //                @Override
